b4e040507d9e6cccb74f83dcaaebeccec0a0a4e9
Heated-Yeti <23yatharth@gmail.com>
2025-05-22 12:06:45 +0530

    Fixed scrape onion, now scrapes ahmia

--- dar web/scraponion.py
+++ dar web/scraponion.py
@@ -2,9 +2,9 @@ import requests
 import re
 
 def scrape_ahmia_urls():
-    url = "https://ahmia.fi/"  # fixed URL
+    url = "https://ahmia.fi/onions/"  # fixed URL
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
     }
 
     print("[*] Scraping .onion URLs from Ahmia...")
@@ -14,7 +14,7 @@ def scrape_ahmia_urls():
         onion_links = re.findall(r'http://[a-zA-Z0-9]{16,56}\.onion', response.text)
         onion_links = list(set(onion_links))  # Remove duplicates
 
-        with open("urls.txt", "w") as f:
+        with open("All_URLs.txt", "w") as f:
             for link in onion_links:
                 f.write(link + "\n")
 
